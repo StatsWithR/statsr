c2c2da1c06b0807af39d73bb0c18c99c31508c83
mine-cetinkaya-rundel <mine@stat.duke.edu>
2016-05-01 01:30:51 -0400

    new function for resampling

---
+++ R/rep_sample_n.R
@@ -0,0 +1,18 @@
+#' Repeating sampling.
+#' 
+#' @param tbl tbl of data.
+#' @param size The number of rows to select.
+#' @param replace Sample with or without replacement?
+#' @param reps The number of samples to collect.
+#' @return A tbl_df that aggregates all created samples, with the addition of a \code{replicate} column that the tbl_df is also grouped by
+#' @export
+
+rep_sample_n <- function(tbl, size, replace = FALSE, reps = 1)
+{
+  n <- nrow(tbl)
+  i <- replicate(reps, sample.int(n, size, replace = replace), simplify = FALSE) %>% unlist()
+  
+  rep_tbl <- cbind(replicate = rep(1:reps,rep(size,reps)), tbl[i,])
+  
+  group_by(rep_tbl, replicate)
+}

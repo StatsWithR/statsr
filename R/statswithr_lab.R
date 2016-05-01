3e37fd569b43c737bd3af5e30925afe12fb3632e
Colin Rundel <rundel@gmail.com>
2016-04-30 22:19:23 -0400

    Update CSS

--- R/statswithr_lab.R
+++ R/statswithr_lab.R
@@ -1,12 +1,13 @@
-statswithr_lab <- function() {
+statswithr_lab = function(...) {
 
   # get the locations of resource files located within the package
-  css <- system.file("lab.css", package = "statsr")
+  css = system.file("lab.css", package = "statsr")
 
   # call the base html_document function
   rmarkdown::html_document(css = css, 
                            highlight = "pygments",
                            theme = "cerulean",
                            fig_width = 7,
-                           fig_height = 4)
+                           fig_height = 4,
+                           ...)
 }

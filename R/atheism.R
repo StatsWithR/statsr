2d25c46c648cf04a67a7235d5c387c9f87319fc5
mine-cetinkaya-rundel <mine@stat.duke.edu>
2016-05-09 11:59:04 -0400

    atheism data

---
+++ R/atheism.R
@@ -0,0 +1,13 @@
+#' Atheism in the world data
+#'
+#' Survey results on atheism across several countries and years. Each row
+#' represents a single respondent.
+#'
+#' @format A tbl_df with 88032 rows and 3 variables:
+#' \describe{
+#'   \item{nationality}{Country of the individual surveyed.}
+#'   \item{response}{A categorical variable with two levels: atheist and non-atheist.}
+#'   \item{year}{Year in which the person was surveyed.}
+#'   }
+#' @source \href{http://www.wingia.com/web/files/richeditor/filemanager/Global_INDEX_of_Religiosity_and_Atheism_PR__6.pdf}{WIN-Gallup International Press Release}
+"atheism"

bb0021a3c38e0785ed00e2ba8def889162f16b4e
Colin Rundel <rundel@gmail.com>
2016-06-07 12:48:30 -0400

    Basics of wage data

---
+++ R/wage.R
@@ -0,0 +1,25 @@
+#' Wage data ...
+#'
+#' Data set contains ...
+#'
+#' @format A tbl_df with with 935 rows and 17 variables:
+#' \describe{
+#'   \item{wage}{}
+#'   \item{hours}{}
+#'   \item{iq}{}
+#'   \item{kww}{}
+#'   \item{educ}{}
+#'   \item{exper}{}
+#'   \item{tenure}{}
+#'   \item{age}{}
+#'   \item{married}{}
+#'   \item{black}{}
+#'   \item{south}{}
+#'   \item{urban}{}
+#'   \item{sibs}{}
+#'   \item{brthord}{}
+#'   \item{meduc}{}
+#'   \item{feduc}{}
+#'   \item{lwage}{}
+#' }
+#' @source ...
